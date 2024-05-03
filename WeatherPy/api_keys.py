2e82823babc7ac08ee272e922854dbefc1812d4f
HGShelton <hannahgradyshelton@gmail.com>
2024-05-03 15:46:35 -0400

    Remove api_keys.py from tracking

--- WeatherPy/api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = “c67c6eed0a72732c6a0d877c144831ea”
-Username = “hmgrady"
-Password = “EmO1120!”
-
-# Geoapify API Key
-geoapify_key = "adcda3e7569b42a6ac42f02db0516fab"
