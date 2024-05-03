ac05675d48fce901e221eec7b4149c5321ffff81
HGShelton <hannahgradyshelton@gmail.com>
2024-05-03 15:38:29 -0400

    Updated api keys and gitignore.

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = “c67c6eed0a72732c6a0d877c144831ea”
+Username = “hmgrady"
+Password = “EmO1120!”
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "adcda3e7569b42a6ac42f02db0516fab"
